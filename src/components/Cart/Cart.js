b91514e8775f155cfd77e9fda787c8b9041277ec
Aman Thukral <thukral618@gmail.com>
2021-01-19 14:37:47 +0530

    formatting cart page

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 import useStyles from './styles';
+import CartItem from './CartItem/CartItem';
 const Cart = ({ cart }) => {
     
     const classes=useStyles();
@@ -13,7 +14,7 @@ const Cart = ({ cart }) => {
         <Grid container spacing={3}>
             {cart.line_items.map((item)=>(
                 <Grid item xs={12} sm={4} key={item.id}>
-                    <div>{item.name}</div>
+                    <CartItem item={item} />
                 </Grid>
             ))}
         </Grid>
@@ -34,7 +35,7 @@ const Cart = ({ cart }) => {
     return (
         <Container>
             <div className={classes.toolbar} />
-            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant="h3" gutterBottom>Your Shopping Cart</Typography>
             { !cart.total_unique_items ? <EmptyCart /> : <FilledCart />}
         </Container>
     )
