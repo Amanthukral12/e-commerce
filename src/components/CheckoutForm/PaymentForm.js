1f6a28b5b8970d9f7d477504ad4828c65d64bf19
Aman Thukral <thukral618@gmail.com>
2021-01-22 03:13:22 +0530

    Creating Review Page

--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import { Typography, Button, Divider } from '@material-ui/core';
+import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import Review from './Review';
 const PaymentForm = () => {
     return (
-        <div>
-           Payment form 
-        </div>
+        <>
+        <Review />
+        </>
     )
 }
 
---
+++ src/components/CheckoutForm/Review.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Typography, List, ListItem, ListItemText } from '@material-ui/core';
+const Review = () => {
+    return (
+        <>
+         <Typography variant="h6" gutterBottom>Order Summary</Typography>   
+        </>
+    )
+}
+
+export default Review
