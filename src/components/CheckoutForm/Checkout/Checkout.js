c3470ffdcdccddbe4f7432df04ba0396bdde3ab3
Aman Thukral <thukral618@gmail.com>
2021-01-22 14:33:55 +0530

    working on Review page

--- src/components/CheckoutForm/Checkout/Checkout.js
+++ src/components/CheckoutForm/Checkout/Checkout.js
@@ -15,7 +15,6 @@ const Checkout = ({ cart }) => {
         const generateToken = async () => {
             try {
                 const token = await commerce.checkout.generateToken(cart.id, { type: 'cart '});
-                console.log(token);
                 setCheckoutToken(token);
             } catch (error) {
 
@@ -40,7 +39,7 @@ const Checkout = ({ cart }) => {
 
     const Form = () => activeStep ===0
     ? <AddressForm checkoutToken={checkoutToken} next={next} />
-    : <PaymentForm shippingData={shippingData} />
+    : <PaymentForm shippingData={shippingData} checkoutToken={checkoutToken} />
     return (
         <>
             <div className={classes.toolbar} />
--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -3,10 +3,10 @@ import { Typography, Button, Divider } from '@material-ui/core';
 import { Elements, CardElement, ElementsConsumer } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import Review from './Review';
-const PaymentForm = () => {
+const PaymentForm = ({ checkoutToken }) => {
     return (
         <>
-        <Review />
+        <Review checkoutToken={checkoutToken} />
         </>
     )
 }
